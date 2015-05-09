188cd706767dcc15a5b4d34ba6833ab62924f390
Erik Martino <erik.martino@gmail.com>
2015-05-09 16:50:21 +0200

    eclipse warning removal

--- ivybridge-facade/src/test/java/dk/profundo/ivybridge/IvyBridgeOptionsTest.java
+++ ivybridge-facade/src/test/java/dk/profundo/ivybridge/IvyBridgeOptionsTest.java
@@ -1,11 +1,5 @@
 package dk.profundo.ivybridge;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-import java.net.URISyntaxException;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
