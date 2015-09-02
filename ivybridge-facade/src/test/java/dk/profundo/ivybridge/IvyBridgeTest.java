71bc402ffad179f10a19472edccca59cfb569d39
Erik Martino <erik.martino@gmail.com>
2015-09-02 17:33:09 +0200

    testcase

--- ivybridge-facade/src/test/java/dk/profundo/ivybridge/IvyBridgeTest.java
+++ ivybridge-facade/src/test/java/dk/profundo/ivybridge/IvyBridgeTest.java
@@ -4,11 +4,20 @@
 package dk.profundo.ivybridge;
 
 import static dk.profundo.ivybridge.IvyTestRepo.opts;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.io.ByteArrayInputStream;
 import java.net.URI;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
+import org.apache.maven.model.Dependency;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -72,10 +81,24 @@ public class IvyBridgeTest {
         opts.getToivy().put("2", "rev:1.0.0-SNAPSHOT=I-1.0.0");
         opts.getFromivy().put("2", "rev:I-1.0.0=1.0.0-SNAPSHOT");
         ivyBridge = new IvyBridgeImpl(opts);
-        final byte[] pomContent = ivyBridge.getPomContent("dk.profundo.maven.ivyrepo", "main-module", "1.0.0-SNAPSHOT", "trunk",
+        byte[] pomContent = ivyBridge.getPomContent("dk.profundo.maven.ivyrepo", "main-module", "1.0.0-SNAPSHOT", "trunk",
                 "runtime");
-        String s = new String(pomContent);
-        assertTrue(s.indexOf("I-1") < 0);
-        System.out.println(s);
+        System.out.println(new String(pomContent));
+        
+        MavenXpp3Reader mavenReader = new MavenXpp3Reader();
+        Model model = mavenReader.read(new ByteArrayInputStream(pomContent));
+        Assert.assertEquals("1.0.0-SNAPSHOT", model.getVersion());
+        
+        List<Dependency> dependencies = model.getDependencies();
+        assertEquals(2,dependencies.size());
+        
+        Map<String,Dependency> deps = new TreeMap<String,Dependency>();
+        for(Dependency d : dependencies) {
+            deps.put(d.getArtifactId(), d);
+        }
+        
+        assertEquals(deps.get("dependent-module").getVersion(),"1.0.0-SNAPSHOT");
+        assertEquals(deps.get("junit").getScope(),"test");
+        
     }
 }
